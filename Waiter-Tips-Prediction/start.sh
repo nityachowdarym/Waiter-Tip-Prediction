a811d29d77dc8268f692059c6d698bd626437ba3
nityachowdarym <110445371+nityachowdarym@users.noreply.github.com>
2025-01-15 12:21:07 -0600

    Add files via upload

---
+++ Waiter-Tips-Prediction/start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+cd ./app/Waiter-Tips-Prediction
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/ubuntu/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
+brew install prometheus
+brew services restart prometheus
+cp -f /home/ubuntu/app/Waiter-Tips-Prediction/prometheus-config.yml /home/linuxbrew/.linuxbrew/etc/prometheus.yml
+brew services restart prometheus
+brew services info prometheus
+sleep 5
+sudo systemctl status postgresql.service --no-pager
+sleep 5
+sudo systemctl status grafana-server --no-pager
+sleep 5
+pip install chardet==4.0.0
+pip install requests -U
+pip install -U click
+pip uninstall Flask-WTF -y
+pip uninstall  WTForms -y
+pip install Flask-WTF==0.15.1
+pip install  WTForms==2.3.3
+echo 'export AIRFLOW_HOME=/home/ubuntu/app/Waiter-Tips-Prediction'
+sudo -i -u postgres
+psql \
+   --host=wtp-rds-instance.cmpdlb9srhwd.eu-west-1.rds.amazonaws.com  \
+   --port=5432 \
+   --username=postgres \
+   --password \
+   --dbname=mlflow_db <<EOF
+CREATE DATABASE mlflow;
+CREATE DATABASE airflow;
+CREATE DATABASE evidently;
+\l
+EOF
---
+++ Waiter-Tips-Prediction/terraform/userdata.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+sudo apt update -y
+cd /home/ubuntu
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.9 -y
+alias python3=python3.9
+sudo apt install awscli -y
+mkdir app
+cd ./app
+mkdir Waiter-Tips-Prediction
+cd ..
+sudo chmod -R 707 /home/ubuntu/app
+echo 'export PATH="${HOME}/app/Waiter-Tips-Prediction:${PATH}"' >> .bashrc
+echo 'alias python3=python3.9' >> .bashrc
+echo 'export PGPASSWORD=password' >> .bashrc
+source .bashrc
+sudo apt install python3-pip -y
+aws s3 cp s3://s3b-tip-predictor/config/requirements.txt ./app/Waiter-Tips-Prediction
+aws s3 cp s3://s3b-tip-predictor/config/prometheus-config.yml ./app/Waiter-Tips-Prediction
+aws s3 cp s3://s3b-tip-predictor/config/start.sh ./app/Waiter-Tips-Prediction
+chmod +x ./app/Waiter-Tips-Prediction/start.sh
+cd ./app/Waiter-Tips-Prediction
+python3.9 -m pip install flask
+pip install -r requirements.txt
+sudo apt-get install build-essential procps curl file git -y
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+sudo wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install grafana -y
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server.service
+sudo apt install -y postgresql postgresql-contrib
+sudo systemctl start postgresql.service
+cd ../../
+echo 'export AIRFLOW_HOME=/home/ubuntu/app/Waiter-Tips-Prediction' >> .bashrc
+source .bashrc
